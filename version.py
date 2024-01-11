1a156ea507157c9be44a60a9d18ffd0e5f975cc7
Dmitriev Andrey <dmitriev.andrey@customscard.ru>
2024-01-11 17:18:21 +0300

    Add some project documentation (#13)

--- version.py
+++ version.py
@@ -9,7 +9,7 @@ class Version:
 
 
 # Change application version here
-VERSION = {'major': 1, 'minor': 0, 'patch': 1}
+VERSION = {'major': 1, 'minor': 0, 'patch': 2}
 
 VERSION_INFO = Version(**VERSION)
 
